86b3a3d5325f0346de07c7df93a52a6e616f68e0
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2024-11-07 06:38:33 +0530

    Views-Patients

--- healthdb/website/urls.py
+++ healthdb/website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name="home"),
+   path('patients', views.patients, name="patients"),
 ]
--- healthdb/website/views.py
+++ healthdb/website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
+def patients(request):
+    return render(request, 'patients.html', {})
